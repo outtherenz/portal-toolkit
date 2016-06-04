957af39231ab17a036cdad3e7f93543c4442a024
Carl Bennett <carlbennettnz@gmail.com>
2016-06-04 21:29:10 +1200

    Update dependencies and switch back to the official ember-font-awesome

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,8 +4,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   const app = new EmberAddon(defaults, {
-    // Add options here
-    hinting: true
+    hinting: false
   });
 
   /*
