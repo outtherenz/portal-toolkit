3049ccde383258efaa1d849d0c702b7e859c2e59
Carl Bennett <carlbennettnz@gmail.com>
2016-06-06 00:28:35 +1200

    Add select() test helper from emberx-select

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,6 @@
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-mocha';
+import registerSelectHelper from './helpers/register-select-helper';
 
 setResolver(resolver);
+registerSelectHelper();
