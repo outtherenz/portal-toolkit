9f6920451a7f586bf6ac90c6474d1eebd366dbf3
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-01 13:58:29 +1200

    initial tests

--- tests/integration/components/radio-slider-test.js
+++ tests/integration/components/radio-slider-test.js
@@ -3,26 +3,94 @@ import { describeComponent, it } from 'ember-mocha';
 import hbs from 'htmlbars-inline-precompile';
 
 describeComponent('radio-slider', 'Integration: RadioSliderComponent', { integration: true }, function() {
-  it.only('renders', function() {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
-    // Template block usage:
-    // this.render(hbs`
-    //   {{#radio-slider}}
-    //     template content
-    //   {{/radio-slider}}
-    // `);
-
-    const radioConst = [
-      { label: 'Month', value: 'month' },
-      { label: 'YTD', value: 'ytd' }
+  it('renders', function() {
+    var radioContent = [
+     { label: 'Month', value: 'month' },
+     { label: 'YTD', value: 'ytd' }
     ];
+    var periodType = 'month';
+    this.set('radioContent', radioContent);
 
-    const periodConst = 'month';
+    this.set('periodType', periodType);
 
-    this.set('radioContent', radioConst);
-    this.set('periodType', periodConst);
-    this.render(hbs`{{radio-slider selected=periodType options=radioContent }}`);
-    expect(this.$()).to.have.lengthOf(1);
+    this.render(hbs` {{radio-slider options=radioContent selected=periodType}}`);
+    expect(this.$('.radio-wrapper')).to.have.lengthOf(1);
+  });
+  it('clicking stuff works', function() {
+    var radioContent = [
+     { label: 'Minutes', value: 'min' },
+     { label: 'Hours', value: 'hour' },
+     { label: 'Days', value: 'day' },
+     { label: 'Weeks', value: 'week' },
+     { label: 'Month', value: 'month' },
+     { label: 'YTD', value: 'ytd' }
+    ];
+    var periodType = 'month';
+    this.set('radioContent', radioContent);
+
+    this.set('periodType', periodType);
+
+    this.render(hbs` {{radio-slider options=radioContent selected=periodType}}`);
+    this.$('#ytd').click();
+    this.$('#min').click();
+    this.$('#day').click();
+    expect(this.get('periodType')).to.equal('day');
+
+    this.$('#hour').click();
+    expect(this.get('periodType')).to.equal('hour');
+  });
+  it('initial value is initial value', function() {
+    var radioContent = [
+     { label: 'Minutes', value: 'min' },
+     { label: 'Hours', value: 'hour' },
+     { label: 'Days', value: 'day' },
+     { label: 'Weeks', value: 'week' },
+     { label: 'Month', value: 'month' },
+     { label: 'YTD', value: 'ytd' }
+    ];
+    var periodType = 'day';
+    this.set('radioContent', radioContent);
+
+    this.set('periodType', periodType);
+
+    this.render(hbs` {{radio-slider options=radioContent selected=periodType}}`);
+    expect(this.get('periodType')).to.equal('day');
+  });
+  it('two sliders work on one page', function() {
+    var radioContent = [
+     { label: 'Minutes', value: 'min' },
+     { label: 'Hours', value: 'hour' },
+     { label: 'Days', value: 'day' },
+     { label: 'Weeks', value: 'week' },
+     { label: 'Month', value: 'month' },
+     { label: 'YTD', value: 'ytd' }
+    ];
+    var secondaryContent = [
+     { label: 'MinutesTest', value: 'minTest' },
+     { label: 'HoursTest', value: 'hourTest' },
+     { label: 'DaysTest', value: 'dayTest' },
+     { label: 'WeeksTest', value: 'weekTest' },
+     { label: 'MonthTest', value: 'monthTest' },
+     { label: 'YTDTest', value: 'ytdTest' }
+    ];
+    var periodType = 'day';
+    var secondaryType = 'dayTest';
+    this.set('radioContent', radioContent);
+    this.set('periodType', periodType);
+    this.set('secondaryContent', secondaryContent);
+    this.set('secondaryType', secondaryType);
+
+    this.render(hbs`
+       {{radio-slider options=radioContent selected=periodType}}
+       {{radio-slider options=secondaryContent selected=secondaryType}}
+    `);
+    expect(this.get('periodType')).to.equal('day');
+    expect(this.get('secondaryType')).to.equal('dayTest');
+    this.$('#min').click();
+    expect(this.get('periodType')).to.equal('min');
+    expect(this.get('secondaryType')).to.equal('dayTest');
+    this.$('#monthTest').click();
+    expect(this.get('periodType')).to.equal('min');
+    expect(this.get('secondaryType')).to.equal('monthTest');
   });
 });
