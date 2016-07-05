fefb02e5d4805a0ad9f54aef1b717af715f8d579
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-06 10:41:05 +1200

    adding more to dummy page

--- tests/dummy/app/controllers/app-components/charts.js
+++ tests/dummy/app/controllers/app-components/charts.js
@@ -94,5 +94,6 @@ export default Controller.extend({
       ]
     }
   ],
-  period: {start: '2016-03', end: '2016-03', type: 'month'}
+  period: {start: '2016-03', end: '2016-03', type: 'month'},
+  series: []
 });
