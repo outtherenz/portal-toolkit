a57142a5791bd49ed956809cb63d7519af44f41e
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-07 11:14:21 +1200

    updated math dummy

--- tests/dummy/app/controllers/helpers/math.js
+++ tests/dummy/app/controllers/helpers/math.js
@@ -1,9 +1,24 @@
 import Ember from 'ember';
 
-const { Controller } = Ember;
+const { Controller, computed } = Ember;
 
 export default Controller.extend({
-  operand1: '1',
-  operator: '+',
-  operand2: 1
+  operand1: 1,
+  operand2: 1,
+  operatorunselect: 1,
+  operator: Ember.computed('operatorunselect', function() {
+    switch (this.get('operatorunselect')) {
+      case '1':
+        return '+';
+      case '2':
+        return '-';
+      case '3':
+        return '*';
+      case '4':
+        return '/';
+      default:
+        return '+';
+    }
+
+  }),
 });
