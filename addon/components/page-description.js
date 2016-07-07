fb4fce48e2472985f21889859c60217374df0620
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-08 11:48:59 +1200

    changed descriptionbox to a block helper

--- addon/components/page-description.js
+++ addon/components/page-description.js
@@ -2,12 +2,5 @@ import Ember from 'ember';
 import layout from '../templates/components/page-description';
 
 export default Ember.Component.extend({
-  layout,
-  h1:'',
-  p1:'',
-  h2:'',
-  p2:'',
-  h3:'',
-  p3:'',
-  pre:''
+  layout
 });
