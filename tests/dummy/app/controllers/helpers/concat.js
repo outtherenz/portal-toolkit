955b0fa6424dbb8c19e2b79c0925ef0282c2fff7
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-07 14:39:53 +1200

    concat helper

---
+++ tests/dummy/app/controllers/helpers/concat.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+const { Controller, computed } = Ember;
+
+export default Controller.extend({
+  word1: 'test word 1',
+  word2: 'test word 2',
+  word3: 'test word 3',
+  word4: 'test word 4',
+  word5: 'test word 5',
+  word6: 'test word 6'
+});
