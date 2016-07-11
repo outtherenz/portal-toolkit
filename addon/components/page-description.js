c6f174912bcadd4a28499222d08a2aa0e2a65976
Nathan <genericusernamee@gmail.com>
2016-07-11 15:25:27 +1200

    Updated radio-slider, page desc & dummy style

--- addon/components/page-description.js
+++ addon/components/page-description.js
@@ -3,5 +3,12 @@ import layout from '../templates/components/page-description';
 
 export default Ember.Component.extend({
   layout,
-  title: ''
+  description: false,
+  title: '',
+
+  actions: {
+    toggleDescription() {
+      this.toggleProperty('description');
+    }
+  }
 });
