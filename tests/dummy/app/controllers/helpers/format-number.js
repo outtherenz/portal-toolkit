aa6dc9616c7d971065ac2475945243a178ea6c23
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-13 16:11:01 +1200

    Wip on format number helper

--- tests/dummy/app/controllers/helpers/format-number.js
+++ tests/dummy/app/controllers/helpers/format-number.js
@@ -3,13 +3,98 @@ import Ember from 'ember';
 const { Controller, computed } = Ember;
 
 export default Controller.extend({
-  formulae1: "{{format-number 'number' 123.456}}",
-  formulae2: "{{format-number 'number' 123.456 places=3}}",
-  formulae3: "{{format-number 'number' -123.456}}",
-  formulae4: "{{format-number 'integer' 3.5}}",
-  formulae5: "{{format-number 'integer' 31235134.123}}",
-  formulae6: "{{format-number 'integer' 31235134.123 sigfigs=4}}",
-  formulae7: "{{format-number 'currency' 3.5}}",
-  formulae8: "{{format-number 'percentage' 0.547}}",
-  formulae9: "{{format-number 'percentage' 0.547 places=1}}"
+  thisExample:{
+    original: 12345.6789,
+    format:"number",
+    places:1,
+    sigfigs:1
+  },
+  numberExamples: [
+    {
+      original: 12345.6789,
+      format:"number",
+      places:"",
+      sigfigs:"",
+      equation:"{{format-number 'number' 12345.6789}}"
+    },{
+      original: 12345.6789,
+      format:"number",
+      places:"",
+      sigfigs:1,
+      equation:"{{format-number 'number' 12345.6789 sigfigs=2}}"
+    },{
+      original: 12345.6789,
+      format:"number",
+      places:1,
+      sigfigs:"",
+      equation:"{{format-number 'number' 12345.6789 places=3}}"
+    },{
+      original: 12345.6789,
+      format:"number",
+      places:1,
+      sigfigs:1,
+      equation:"{{format-number 'number' 12345.6789 sigfigs=2 places=2}}"
+    },{
+      original: 12345.6789,
+      format:"number",
+      places:5,
+      sigfigs:"",
+      equation:"{{format-number 'number' 12345.6789 places=5}}"
+    },{
+      original: 12345.6789,
+      format:"number",
+      places:"",
+      sigfigs:5,
+      equation:"{{format-number 'number' 12345.6789 sigfigs=5}}"
+    },{
+      original: 12345.6789,
+      format:"number",
+      places:5,
+      sigfigs:5,
+      equation:"{{format-number 'number' 12345.6789 sigfigs=5 places=5}}"
+    }],
+    currencyExamples: [
+      {
+        original: 12345.6789,
+        format:"currency",
+        places:"",
+        sigfigs:"",
+        equation:"{{format-number 'number' 12345.6789}}"
+      },{
+        original: 12345.6789,
+        format:"currency",
+        places:"",
+        sigfigs:1,
+        equation:"{{format-number 'number' 12345.6789 sigfigs=2}}"
+      },{
+        original: 12345.6789,
+        format:"currency",
+        places:1,
+        sigfigs:"",
+        equation:"{{format-number 'number' 12345.6789 places=3}}"
+      },{
+        original: 12345.6789,
+        format:"currency",
+        places:1,
+        sigfigs:1,
+        equation:"{{format-number 'number' 12345.6789 sigfigs=2 places=2}}"
+      },{
+        original: 12345.6789,
+        format:"currency",
+        places:5,
+        sigfigs:"",
+        equation:"{{format-number 'number' 12345.6789 places=5}}"
+      },{
+        original: 12345.6789,
+        format:"currency",
+        places:"",
+        sigfigs:5,
+        equation:"{{format-number 'number' 12345.6789 sigfigs=5}}"
+      },{
+        original: 12345.6789,
+        format:"currency",
+        places:5,
+        sigfigs:5,
+        equation:"{{format-number 'number' 12345.6789 sigfigs=5 places=5}}"
+      }],
 });
