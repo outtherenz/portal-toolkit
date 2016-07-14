9959c096631fbbb154055fbc4d0522c9687a19f0
Nathan <genericusernamee@gmail.com>
2016-07-14 15:46:08 +1200

    more linting

--- tests/dummy/app/controllers/app-components/value-change-indicator.js
+++ tests/dummy/app/controllers/app-components/value-change-indicator.js
@@ -3,28 +3,28 @@ import Ember from 'ember';
 const { Controller } = Ember;
 
 export default Controller.extend({
-  codeSample1: "{{value-change-indicator difference=target.monthDifference from=target.from to=target.to useColor=target.useColor increaseIsGood=target.increaseIsGood}}",
+  codeSample1: '{{value-change-indicator difference=target.monthDifference from=target.from to=target.to useColor=target.useColor increaseIsGood=target.increaseIsGood}}',
   targets: [
     {
-      'monthDifference': 3,
-      'useColor': true,
-      'increaseIsGood': true
-    },{
-      'to': 8,
-      'from': -10,
-      'useColor': true,
-      'increaseIsGood': true
-    },{
-      'monthDifference': -3,
-      'to': 8,
-      'from': 10,
-      'useColor': true,
-      'increaseIsGood': false
-    },{
-      'to': 8,
-      'from': 10,
-      'useColor': true,
-      'increaseIsGood': false
+      monthDifference: 3,
+      useColor: true,
+      increaseIsGood: true
+    }, {
+      to: 8,
+      from: -10,
+      useColor: true,
+      increaseIsGood: true
+    }, {
+      monthDifference: -3,
+      to: 8,
+      from: 10,
+      useColor: true,
+      increaseIsGood: false
+    }, {
+      to: 8,
+      from: 10,
+      useColor: true,
+      increaseIsGood: false
     }
   ],
 
--- tests/dummy/app/controllers/helpers/format-number.js
+++ tests/dummy/app/controllers/helpers/format-number.js
@@ -1,166 +1,163 @@
 import Ember from 'ember';
 
-const { Controller, computed } = Ember;
+const { Controller } = Ember;
 
 export default Controller.extend({
-  formats:[
+  formats: [
     {name: 'Number Examples',
       examples: [
-      {
-        original: 12345.6789,
-        format: 'number',
-        places: null,
-        sigfigs: null,
-        equation: "{{format-number 'number' 12345.6789}}"
-      },{
-        original: 12345.6789,
-        format: 'number',
-        places: null,
-        sigfigs: 1,
-        equation: "{{format-number 'number' 12345.6789 sigfigs=2}}"
-      },{
-        original: 12345.6789,
-        format: 'number',
-        places: 1,
-        sigfigs: null,
-        equation: "{{format-number 'number' 12345.6789 places=3}}"
-      },{
-        original: 12345.6789,
-        format: 'number',
-        places: 1,
-        sigfigs: 1,
-        equation: "{{format-number 'number' 12345.6789 sigfigs=2 places=2}}"
-      },{
-        original: 12345.6789,
-        format: 'number',
-        places: 5,
-        sigfigs: null,
-        equation: "{{format-number 'number' 12345.6789 places=5}}"
-      },{
-        original: 12345.6789,
-        format: 'number',
-        places: null,
-        sigfigs: 5,
-        equation: "{{format-number 'number' 12345.6789 sigfigs=5}}"
-      },{
-        original: 12345.6789,
-        format: 'number',
-        places: 5,
-        sigfigs: 5,
-        equation: "{{format-number 'number' 12345.6789 sigfigs=5 places=5}}"
-      }
-    ]},  {name: 'Percentage Examples', examples: [
-      {
-        original: 12345.6789,
-        format: 'percentage',
-        places: null,
-        sigfigs: null,
-        equation: "{{format-number 'percentage' 12345.6789}}"
-      },{
-        original: 12345.6789,
-        format: 'percentage',
-        places: null,
-        sigfigs: 1,
-        equation: "{{format-number 'percentage' 12345.6789 sigfigs=2}}"
-      },{
-        original: 12345.6789,
-        format: 'percentage',
-        places: 1,
-        sigfigs: null,
-        equation: "{{format-number 'percentage' 12345.6789 places=3}}"
-      },{
-        original: 12345.6789,
-        format: 'percentage',
-        places: 1,
-        sigfigs: 1,
-        equation: "{{format-number 'percentage' 12345.6789 sigfigs=2 places=2}}"
-      },{
-        original: 12345.6789,
-        format: 'percentage',
-        places: 5,
-        sigfigs: null,
-        equation: "{{format-number 'percentage' 12345.6789 places=5}}"
-      },{
-        original: 12345.6789,
-        format: 'percentage',
-        places: null,
-        sigfigs: 5,
-        equation: "{{format-number 'percentage' 12345.6789 sigfigs=5}}"
-      },{
-        original: 12345.6789,
-        format: 'percentage',
-        places: 5,
-        sigfigs: 5,
-        equation: "{{format-number 'percentage' 12345.6789 sigfigs=5 places=5}}"
-      }]}, {name: 'Currency Examples', examples: [
         {
           original: 12345.6789,
-          format: 'currency',
+          format: 'number',
           places: null,
           sigfigs: null,
-          equation: "{{format-number 'currency' 12345.6789}}"
-        },{
+          equation: "{{format-number 'number' 12345.6789}}"
+        }, {
           original: 12345.6789,
-          format: 'currency',
+          format: 'number',
           places: null,
           sigfigs: 1,
-          equation: "{{format-number 'currency' 12345.6789 sigfigs=2}}"
-        },{
+          equation: "{{format-number 'number' 12345.6789 sigfigs=2}}"
+        }, {
           original: 12345.6789,
-          format: 'currency',
+          format: 'number',
           places: 1,
           sigfigs: null,
-          equation: "{{format-number 'currency' 12345.6789 places=3}}"
-        },{
+          equation: "{{format-number 'number' 12345.6789 places=3}}"
+        }, {
           original: 12345.6789,
-          format: 'currency',
+          format: 'number',
           places: 1,
           sigfigs: 1,
-          equation: "{{format-number 'currency' 12345.6789 sigfigs=2 places=2}}"
-        },{
+          equation: "{{format-number 'number' 12345.6789 sigfigs=2 places=2}}"
+        }, {
           original: 12345.6789,
-          format: 'currency',
+          format: 'number',
           places: 5,
           sigfigs: null,
-          equation: "{{format-number 'currency' 12345.6789 places=5}}"
-        },{
+          equation: "{{format-number 'number' 12345.6789 places=5}}"
+        }, {
           original: 12345.6789,
-          format: 'currency',
+          format: 'number',
           places: null,
           sigfigs: 5,
-          equation: "{{format-number 'currency' 12345.6789 sigfigs=5}}"
-        },{
+          equation: "{{format-number 'number' 12345.6789 sigfigs=5}}"
+        }, {
           original: 12345.6789,
-          format: 'currency',
+          format: 'number',
           places: 5,
           sigfigs: 5,
-          equation: "{{format-number 'currency' 12345.6789 sigfigs=5 places=5}}"
-        }]}],
-  numberExample:{
+          equation: "{{format-number 'number' 12345.6789 sigfigs=5 places=5}}"
+        }
+      ]},  {name: 'Percentage Examples', examples: [
+        {
+          original: 12345.6789,
+          format: 'percentage',
+          places: null,
+          sigfigs: null,
+          equation: "{{format-number 'percentage' 12345.6789}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: null,
+          sigfigs: 1,
+          equation: "{{format-number 'percentage' 12345.6789 sigfigs=2}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: 1,
+          sigfigs: null,
+          equation: "{{format-number 'percentage' 12345.6789 places=3}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: 1,
+          sigfigs: 1,
+          equation: "{{format-number 'percentage' 12345.6789 sigfigs=2 places=2}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: 5,
+          sigfigs: null,
+          equation: "{{format-number 'percentage' 12345.6789 places=5}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: null,
+          sigfigs: 5,
+          equation: "{{format-number 'percentage' 12345.6789 sigfigs=5}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: 5,
+          sigfigs: 5,
+          equation: "{{format-number 'percentage' 12345.6789 sigfigs=5 places=5}}"
+        }]}, {name: 'Currency Examples', examples: [
+          {
+            original: 12345.6789,
+            format: 'currency',
+            places: null,
+            sigfigs: null,
+            equation: "{{format-number 'currency' 12345.6789}}"
+          }, {
+            original: 12345.6789,
+            format: 'currency',
+            places: null,
+            sigfigs: 1,
+            equation: "{{format-number 'currency' 12345.6789 sigfigs=2}}"
+          }, {
+            original: 12345.6789,
+            format: 'currency',
+            places: 1,
+            sigfigs: null,
+            equation: "{{format-number 'currency' 12345.6789 places=3}}"
+          }, {
+            original: 12345.6789,
+            format: 'currency',
+            places: 1,
+            sigfigs: 1,
+            equation: "{{format-number 'currency' 12345.6789 sigfigs=2 places=2}}"
+          }, {
+            original: 12345.6789,
+            format: 'currency',
+            places: 5,
+            sigfigs: null,
+            equation: "{{format-number 'currency' 12345.6789 places=5}}"
+          }, {
+            original: 12345.6789,
+            format: 'currency',
+            places: null,
+            sigfigs: 5,
+            equation: "{{format-number 'currency' 12345.6789 sigfigs=5}}"
+          }, {
+            original: 12345.6789,
+            format: 'currency',
+            places: 5,
+            sigfigs: 5,
+            equation: "{{format-number 'currency' 12345.6789 sigfigs=5 places=5}}"
+          }]}],
+  numberExample: {
     original: 12345.6789,
     format: 'number',
     places: 1,
     sigfigs: 1
   },
-  currencyExample:{
+  currencyExample: {
     original: 12345.6789,
     format: 'currency',
     places: 1,
     sigfigs: 1
   },
-  integerExample:{
+  integerExample: {
     original: 12345.6789,
     format: 'integer',
     places: 1,
     sigfigs: 1
   },
-  percentageExample:{
+  percentageExample: {
     original: 12345.6789,
     format: 'percentage',
     places: 1,
     sigfigs: 1
-  },
-
-
-
+  }
 });
