d4e38fc1929aa0e06df32eb79abffb7f4d852537
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-15 15:20:56 +1200

    Added error to active table

--- tests/dummy/app/controllers/app-components/active-table.js
+++ tests/dummy/app/controllers/app-components/active-table.js
@@ -18,6 +18,10 @@ export default Controller.extend({
       name: 'Numbers',
       key: 'numbers',
       width: 5
+    }, {
+      name: 'Numbers As Strings',
+      key: 'numbersAsStrings',
+      width: 5
     }];
     return columns;
 
@@ -26,27 +30,33 @@ export default Controller.extend({
   data: [{
     letters: 'a',
     dates: new Date('1984-05-21'),
-    numbers: '1'
+    numbers: 1,
+    numbersAsStrings: '1'
   }, {
     letters: 'A',
     dates: new Date('1987-02-11'),
-    numbers: '2'
+    numbers: 2,
+    numbersAsStrings: '2'
   }, {
     letters: 'a',
     dates: new Date('1987-02-12'),
-    numbers: '2'
+    numbers: 2,
+    numbersAsStrings: '2'
   }, {
     letters: 'b',
     dates: new Date('1986-03-22'),
-    numbers: '12'
+    numbers: 12,
+    numbersAsStrings: '12'
   }, {
     letters: 'B',
     dates: new Date('1987-02-10'),
-    numbers: '01'
+    numbers: 1,
+    numbersAsStrings: '01'
   }, {
     letters: 'ab',
     dates: new Date('1983-04-20'),
-    numbers: '02'
+    numbers: 2,
+    numbersAsStrings: '02'
   }],
 
   tableContent: Ember.computed.sort('data', 'sort')
