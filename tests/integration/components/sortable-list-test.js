1244d1963f13f1089d88bb6aaa5192dfc379d0c6
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-20 08:18:46 +1200

    made tests relevent

--- tests/integration/components/sortable-list-test.js
+++ tests/integration/components/sortable-list-test.js
@@ -15,16 +15,38 @@ describeComponent('sortable-list', 'Integration: SortableListComponent', { integ
     ];
     this.set('content', content);
   });
-  it('renders', function() {
-    this.render(hbs`{{sortable-list content=content property='name' class="block"}}`);
-    expect(this.$('.block li')).to.have.lengthOf(6);
-    expect(this.$('.block li:nth-child(1)')).to.equal('john');
+  it('renders all our content correctly', function() {
+    this.render(hbs`{{sortable-list content=content property='name' class="testClass"}}`);
+    expect(this.$('.testClass li')).to.have.lengthOf(6);
+    expect(this.$('ol.testClass li:nth-child(1)').text()).to.equal('john');
+    expect(this.$('ol.testClass li:nth-child(2)').text()).to.equal('ben');
+    expect(this.$('ol.testClass li:nth-child(3)').text()).to.equal('greg');
+    expect(this.$('ol.testClass li:nth-child(4)').text()).to.equal('fraser');
+    expect(this.$('ol.testClass li:nth-child(5)').text()).to.equal('janyk');
+    expect(this.$('ol.testClass li:nth-child(6)').text()).to.equal('steve');
   });
-  it.only('click and drag part 1', function() {
-    this.render(hbs`{{sortable-list content=content property='name' class="block"}}`);
-    const $location = this.$('.block li:nth-child(1)');
-    $location.mousedown();
-    expect($location.text()).to.equal('john');
-    console.log($location);
+  it('works when not supplied with a class', function() {
+    this.render(hbs`{{sortable-list content=content property='name'}}`);
+    expect(this.$(' li')).to.have.lengthOf(6);
+    expect(this.$('ol li:nth-child(1)').text()).to.equal('john');
+    expect(this.$('ol li:nth-child(2)').text()).to.equal('ben');
+    expect(this.$('ol li:nth-child(3)').text()).to.equal('greg');
+    expect(this.$('ol li:nth-child(4)').text()).to.equal('fraser');
+    expect(this.$('ol li:nth-child(5)').text()).to.equal('janyk');
+    expect(this.$('ol li:nth-child(6)').text()).to.equal('steve');
+  });
+  it('works as expected when not supplied with a name value', function() {
+    this.render(hbs`{{sortable-list content=content class="testClass"}}`);
+    expect(this.$('.testClass li')).to.have.lengthOf(6);
+    expect(this.$('ol.testClass li:nth-child(1)').text()).to.equal('');
+    expect(this.$('ol.testClass li:nth-child(2)').text()).to.equal('');
+    expect(this.$('ol.testClass li:nth-child(3)').text()).to.equal('');
+    expect(this.$('ol.testClass li:nth-child(4)').text()).to.equal('');
+    expect(this.$('ol.testClass li:nth-child(5)').text()).to.equal('');
+    expect(this.$('ol.testClass li:nth-child(6)').text()).to.equal('');
+  });
+  it('works as expected when not supplied with content', function() {
+    this.render(hbs`{{sortable-list property='name' class="testClass"}}`);
+    expect(this.$('.testClass li')).to.have.lengthOf(0);
   });
 });
