edf28ea2aeecec0251bc232fcae57ec4c2b83e5d
JuanPGarcia <juan@outtherebusiness.co.nz>
2016-07-20 12:55:51 +1200

    sarted on checkbox list, added dependencies

--- tests/dummy/app/controllers/app-components/untethered-modal-dialog.js
+++ tests/dummy/app/controllers/app-components/untethered-modal-dialog.js
@@ -4,16 +4,19 @@ const { Controller } = Ember;
 
 export default Controller.extend({
   testing: false,
+  works: false,
   actions: {
     toggleIsConfirmingTest() {
       if (this.get('testing')) {
         this.set('testing', false);
       } else {
         this.set('testing', true);
+        this.set('works', false);
       }
     },
     test() {
-      alert("it works!!");
+      this.set('works', true);
+      this.set('testing', false);
     }
   }
 });
