5717c4d787cdb24e4f60b6b1513712d1ab7e6095
Carl Bennett <carlbennettnz@gmail.com>
2016-07-28 13:25:06 +1200

    Prevent linting tests from being generated

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   const app = new EmberAddon(defaults, {
-    hinting: false
+    hinting: false,
+
+    eslint: {
+      // Prevents linting tests from being automatically generated
+      testGenerator: () => null
+    }
   });
 
   /*
