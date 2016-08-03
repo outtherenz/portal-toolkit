e629d69a16540133bfc2bb35dc30afe117be1dcc
Juan <juan@outtherebusiness.co.nz>
2016-08-03 14:43:48 +1200

    added value checks to charts

--- tests/integration/components/charts-test.js
+++ tests/integration/components/charts-test.js
@@ -36,8 +36,13 @@ describeComponent('charts', 'Integration: ChartsComponent', { integration: true,
 
   it('renders data-table component', function() {
     this.render(hbs`{{charts/data-table series=series thisData=data metrics=metrics period=period}}`);
-    expect(this.$('table tr td').text()).to.equal('Sales');
-    expect(this.$('table tr td').next().text()).to.equal('');
+    expect(this.$('table tr td')[0].firstChild.data).to.equal('Sales');
+    expect(this.$('table tr td.text-right').text()).to.equal('');
+  });
+  it('fills in data-table properly', function() {
+    this.render(hbs`{{charts/data-table series=series thisData=data metrics=metrics period=period}}`);
+    expect(this.$('table tr td')[0].firstChild.data).to.equal('Sales');
+    expect(this.$('table tr td.text-right').text()).to.equal(63);
   });
 
   it('renders line-chart component', function() {
@@ -45,9 +50,18 @@ describeComponent('charts', 'Integration: ChartsComponent', { integration: true,
     expect(this.$('.c3-event-rect-11')).to.have.lengthOf(1);
     expect(this.$('.c3-event-rect-13')).to.not.have.lengthOf(1);
   });
+  it('fills in line-chart data properly', function() {
+    this.render(hbs`{{charts/line-chart series=series thisData=data metrics=metrics period=period}}`);
+    let portion = this.$('.c3-event-rect-11')[0].x.animVal.value;
+    expect(portion).to.equal(500.5);
+  });
 
   it('renders pie-chart component', function() {
     this.render(hbs`{{charts/pie-chart series=series thisData=data metrics=metrics period=period}}`);
     expect(this.$('.c3-arc-Sales')).to.have.lengthOf(1);
+  });
+  it('fills in pie-chart data properly', function() {
+    this.render(hbs`{{charts/pie-chart series=series thisData=data metrics=metrics period=period}}`);
+    expect(this.$('.c3-arc-Sales')[0].__data__.value).to.equal(27064);
   });
 });
