c2a448bf51661a95981835fb410a9677b306904b
Carl Bennett <carlbennettnz@gmail.com>
2016-10-31 11:50:24 +1300

    Fix Mirage helper

--- tests/helpers/setup-mirage-for-integration.js
+++ tests/helpers/setup-mirage-for-integration.js
@@ -1,7 +1,7 @@
 import { startMirage } from '../../initializers/ember-cli-mirage';
 import defaultScenario from '../../../dummy/mirage/scenarios/default';
 
-export default function startMirage(container) {
+export default function startMirageWithDefaults(container) {
   startMirage(container);
   defaultScenario(window.server);
 }
