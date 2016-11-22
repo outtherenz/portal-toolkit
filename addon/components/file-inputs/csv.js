1275d7b9bb2f2278c2e0e756dbe22c57424ccc01
Fraser Croad <fraser.croad@gmail.com>
2016-11-23 11:21:47 +1300

    Removes new line char in last row of file

--- addon/components/file-inputs/csv.js
+++ addon/components/file-inputs/csv.js
@@ -17,7 +17,17 @@ export default EmberUploader.FileField.extend({
 
     Papa.parse(files[0], {
       skipEmptyLines: true,
-      complete: results => set(this, 'file', results)
+      complete: results => {
+        // This is a very strange case that arose in production.
+        // Somehow a new line char was being missed at the end of the file.
+        // Causing it to be in a line by itself and breaking the format.
+        // This may get fixed in subsequent versions of papaparse but for
+        // now, they are not accepting issues.
+        const lastLine = results.data[results.data.length - 1];
+        if (lastLine.length === 1 && lastLine[0] === "\n") results.data.pop();
+
+        set(this, 'file', results);
+      }
     });
   }
 });
