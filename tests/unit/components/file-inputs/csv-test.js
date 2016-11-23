759867f977340182a227b53f4e7d79cbace62f76
Carl Bennett <carlbennettnz@gmail.com>
2016-11-23 13:44:31 +1300

    Add CSV input test for mixed line endings

---
+++ tests/unit/components/file-inputs/csv-test.js
@@ -0,0 +1,32 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import wait from 'ember-test-helpers/wait';
+
+const fileContent = `Company,ReportTitle,Period,Account,Name,MonthBal,YTDBal
+GISB,BALANCE SHEET,201702,TM7701,Manufacturing Stock on Hand,"-4,176.84","17,230.60"
+\r`;
+
+moduleForComponent('file-inputs/csv', 'Unit | Component | file inputs/csv', {
+  // Specify the other units that are required for this test
+  // needs: ['component:foo', 'helper:bar'],
+  unit: true
+});
+
+test('it can handle mixed line endings', function(assert) {
+  assert.expect(1);
+
+  let component = this.subject();
+
+  const file = new File([ fileContent ], 'file-name.csv');
+  const event = {
+    target: {
+      files: [ file ]
+    }
+  }
+  
+  component.change(event);
+
+  return wait().then(() => {
+    const result = component.get('file');
+    assert.equal(result.data.length, 2);
+  });
+});
