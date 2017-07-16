5029ce0fea561feb6b476fb298290b7210a808a2
Fraser Croad <fraser@otbs.co.nz>
2017-07-17 11:48:14 +1200

    Fix tests
    I don't know why they broke or why updating fixes them...

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,6 +1,8 @@
 import resolver from './helpers/resolver';
 import { setResolver } from 'ember-qunit';
 import registerSelectHelper from './helpers/register-select-helper';
+import { start } from 'ember-cli-qunit';
 
 setResolver(resolver);
 registerSelectHelper();
+start();
