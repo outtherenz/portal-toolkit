4a49a05642232411725123c7a888c969e1267dd2
Fraser Croad <fraser@otbs.co.nz>
2017-10-20 12:03:44 +1300

    Fix weird indentation

--- tests/dummy/app/controllers/helpers/format-number.js
+++ tests/dummy/app/controllers/helpers/format-number.js
@@ -5,8 +5,9 @@ const { Controller } = Ember;
 export default Controller.extend({
   closebraces: '}}',
   formats: [
-    {name: 'Number',
-     examples: [
+    {
+      name: 'Number',
+      examples: [
         {
           original: 12345.6789,
           format: 'number',
@@ -50,95 +51,104 @@ export default Controller.extend({
           sigfigs: 5,
           equation: "{{format-number 'number' 12345.6789 sigfigs=5 places=5}}"
         }
-      ]},  {name: 'Percentage', examples: [
-      {
-        original: 12345.6789,
-        format: 'percentage',
-        places: null,
-        sigfigs: null,
-        equation: "{{format-number 'percentage' 12345.6789}}"
-      }, {
-        original: 12345.6789,
-        format: 'percentage',
-        places: null,
-        sigfigs: 1,
-        equation: "{{format-number 'percentage' 12345.6789 sigfigs=2}}"
-      }, {
-        original: 12345.6789,
-        format: 'percentage',
-        places: 1,
-        sigfigs: null,
-        equation: "{{format-number 'percentage' 12345.6789 places=3}}"
-      }, {
-        original: 12345.6789,
-        format: 'percentage',
-        places: 1,
-        sigfigs: 1,
-        equation: "{{format-number 'percentage' 12345.6789 sigfigs=2 places=2}}"
-      }, {
-        original: 12345.6789,
-        format: 'percentage',
-        places: 5,
-        sigfigs: null,
-        equation: "{{format-number 'percentage' 12345.6789 places=5}}"
-      }, {
-        original: 12345.6789,
-        format: 'percentage',
-        places: null,
-        sigfigs: 5,
-        equation: "{{format-number 'percentage' 12345.6789 sigfigs=5}}"
-      }, {
-        original: 12345.6789,
-        format: 'percentage',
-        places: 5,
-        sigfigs: 5,
-        equation: "{{format-number 'percentage' 12345.6789 sigfigs=5 places=5}}"
-      }]},  {name: 'Integer', examples: [
-      {
-        original: 12345.6789,
-        format: 'integer',
-        places: null,
-        sigfigs: null,
-        equation: "{{format-number 'integer' 12345.6789}}"
-      }, {
-        original: 12345.6789,
-        format: 'integer',
-        places: null,
-        sigfigs: 1,
-        equation: "{{format-number 'integer' 12345.6789 sigfigs=2}}"
-      }, {
-        original: 12345.6789,
-        format: 'integer',
-        places: 1,
-        sigfigs: null,
-        equation: "{{format-number 'integer' 12345.6789 places=3}}"
-      }, {
-        original: 12345.6789,
-        format: 'integer',
-        places: 1,
-        sigfigs: 1,
-        equation: "{{format-number 'integer' 12345.6789 sigfigs=2 places=2}}"
-      }, {
-        original: 12345.6789,
-        format: 'integer',
-        places: 5,
-        sigfigs: null,
-        equation: "{{format-number 'integer' 12345.6789 places=5}}"
-      }, {
-        original: 12345.6789,
-        format: 'integer',
-        places: null,
-        sigfigs: 5,
-        equation: "{{format-number 'integer' 12345.6789 sigfigs=5}}"
-      }, {
-        original: 12345.6789,
-        format: 'integer',
-        places: 5,
-        sigfigs: 5,
-        equation: "{{format-number 'integer' 12345.6789 sigfigs=5 places=5}}"
-      }]
+      ]
     }, {
-      name: 'Currency', examples: [
+      name: 'Percentage',
+      examples: [
+        {
+          original: 12345.6789,
+          format: 'percentage',
+          places: null,
+          sigfigs: null,
+          equation: "{{format-number 'percentage' 12345.6789}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: null,
+          sigfigs: 1,
+          equation: "{{format-number 'percentage' 12345.6789 sigfigs=2}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: 1,
+          sigfigs: null,
+          equation: "{{format-number 'percentage' 12345.6789 places=3}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: 1,
+          sigfigs: 1,
+          equation: "{{format-number 'percentage' 12345.6789 sigfigs=2 places=2}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: 5,
+          sigfigs: null,
+          equation: "{{format-number 'percentage' 12345.6789 places=5}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: null,
+          sigfigs: 5,
+          equation: "{{format-number 'percentage' 12345.6789 sigfigs=5}}"
+        }, {
+          original: 12345.6789,
+          format: 'percentage',
+          places: 5,
+          sigfigs: 5,
+          equation: "{{format-number 'percentage' 12345.6789 sigfigs=5 places=5}}"
+        }
+      ]
+    }, {
+      name: 'Integer',
+      examples: [
+        {
+          original: 12345.6789,
+          format: 'integer',
+          places: null,
+          sigfigs: null,
+          equation: "{{format-number 'integer' 12345.6789}}"
+        }, {
+          original: 12345.6789,
+          format: 'integer',
+          places: null,
+          sigfigs: 1,
+          equation: "{{format-number 'integer' 12345.6789 sigfigs=2}}"
+        }, {
+          original: 12345.6789,
+          format: 'integer',
+          places: 1,
+          sigfigs: null,
+          equation: "{{format-number 'integer' 12345.6789 places=3}}"
+        }, {
+          original: 12345.6789,
+          format: 'integer',
+          places: 1,
+          sigfigs: 1,
+          equation: "{{format-number 'integer' 12345.6789 sigfigs=2 places=2}}"
+        }, {
+          original: 12345.6789,
+          format: 'integer',
+          places: 5,
+          sigfigs: null,
+          equation: "{{format-number 'integer' 12345.6789 places=5}}"
+        }, {
+          original: 12345.6789,
+          format: 'integer',
+          places: null,
+          sigfigs: 5,
+          equation: "{{format-number 'integer' 12345.6789 sigfigs=5}}"
+        }, {
+          original: 12345.6789,
+          format: 'integer',
+          places: 5,
+          sigfigs: 5,
+          equation: "{{format-number 'integer' 12345.6789 sigfigs=5 places=5}}"
+        }
+      ]
+    }, {
+      name: 'Currency',
+      examples: [
         {
           original: 12345.6789,
           format: 'currency',
@@ -181,7 +191,10 @@ export default Controller.extend({
           places: 5,
           sigfigs: 5,
           equation: "{{format-number 'currency' 12345.6789 sigfigs=5 places=5}}"
-        }]}],
+        }
+      ]
+    }
+  ],
   numberExample: {
     original: 12345.6789,
     format: 'number',
