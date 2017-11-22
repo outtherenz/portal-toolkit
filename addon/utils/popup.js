cac952730f2d78438b347329bc20b15323875793
Fraser Croad <fraser@otbs.co.nz>
2017-11-23 11:20:12 +1300

    Add annotations

--- addon/utils/popup.js
+++ addon/utils/popup.js
@@ -3,6 +3,27 @@ import qs from 'npm:qs';
 
 const { $, run } = Ember;
 
+/**
+ * This is a wrapper on window.open()
+ * Your popup window should eventually run the code:
+ * ```
+ * window.opener.Ember.$(window.opener).trigger('popup.message', message)
+ * ```
+ * The message can be an object or an URL Encoded string that will be pased by qs.
+ * There is a file that implements this API available at /portal-toolkit/popup-callback.html
+ * (file locatied at this-repo/assets/popup-callback.html) and sends back the query or anchor string as the message.
+ * @example
+ * import popups from 'portal-toolkit/utils/popup';
+ * const {
+ *   popup,
+ *   promise
+ * } = popups('', WindowName, Options); // Create popup window
+ * popup.location.href = URL; // Use reference to window
+ * promise.then(message => use message); // Use message
+ * @param {String} URL - URL of the popup window.
+ * @returns {Object} - Object contains `promise` which resolves the message from the popup
+ * window and `popup` which is a reference to the window created.
+ */
 export default function createPopup() {
   const popup = window.open(...arguments);
 
