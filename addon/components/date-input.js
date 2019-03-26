e71694688c3b24b8ab52cb585e2e2ba57bf20eca
Nathan <genericusernamee@gmail.com>
2019-03-27 11:36:15 +1300

    Give date-input a manual text input

--- addon/components/date-input.js
+++ addon/components/date-input.js
@@ -1,4 +1,8 @@
 import Component from '@ember/component';
+import { next } from '@ember/runloop';
+import moment from 'moment';
+import $ from 'jquery';
+import { set, get, observer } from '@ember/object';
 import layout from '../templates/components/date-input';
 
 export default Component.extend({
@@ -11,10 +15,42 @@ export default Component.extend({
   isActive: false,
   format: 'dddd, D MMMM YYYY',
 
+  didInsertElement() {
+    const element = get(this, 'elementId');
+    $(window).on('keydown keyup click blur', event => {
+      const { target, key } = event;
+      const isTarget = $(`#${element}`).is(target);
+      const hasTarget = $(`#${element}`).has(target).length !== 0;
+
+      if (isTarget || hasTarget) {
+        if (['Enter', 'Escape'].includes(key)) {
+          set(this, 'isActive', false);
+          event.preventDefault();
+        }
+      } else if (get(this, 'isActive')) {
+        set(this, 'isActive', false);
+      }
+    });
+  },
+
+  willDestroyElement() {
+    // remove our listener
+    $(window).off('keydown keyup click blur');
+  },
+
+  focusDateInput: observer('isActive', function() {
+    if (get(this, 'isActive')) next(() => this.$('input').focus());
+  }),
+
   actions: {
     selectAndDeactivate(date) {
       this.toggleProperty('isActive');
       this.sendAction('select', date);
+    },
+    handleDateInput(value) {
+      const date = moment(value, 'YYYY-MM-DD').toDate();
+
+      if (date.getTime()) this.sendAction('select', date);
     }
   }
 });
