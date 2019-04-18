3ae741c3a19becccc850728321033aa7a989c311
FCDevman <fraser@otbs.co.nz>
2019-04-18 14:40:17 +1200

    Fix test for date-picker, remove now-redundant test

--- tests/integration/components/date-input-test.js
+++ tests/integration/components/date-input-test.js
@@ -8,31 +8,19 @@ moduleForComponent('date-input', 'Integration | Component | date input', {
 
 test('clicking the input toggles the picker', function(assert) {
   this.set('date', new Date());
-  this.render(hbs`{{date-input date=date select=(action (mut date))}}`);
+  this.render(hbs`
+    <div class="outside-div">Hello</div>
+    {{date-input date=date select=(action (mut date))}}
+  `);
 
-  assert.equal(this.$('.date-input__picker').length, 0);
+  assert.equal(this.$('.date-input__picker').length, 0, 'Picker has not yet been rendered');
 
-  const display = this.$('.date-input__display');
+  this.$('.date-input__display').click();
 
-  display.click();
+  assert.equal(this.$('.date-input__picker').length, 1, 'Picker has been rendered');
 
-  return wait().then(() => {
-    assert.equal(this.$('.date-input__picker').length, 1);
-    display.click();
-  }).then(() => {
-    assert.equal(this.$('.date-input__picker').length, 0);
-  });
-});
-
-test('clicking the overlay hides the picker', function(assert) {
-  this.set('date', new Date());
-  this.render(hbs`{{date-input date=date isActive=true select=(action (mut date))}}`);
-
-  this.$('.date-input__overlay').click();
-
-  return wait().then(() => {
-    assert.equal(this.$('.date-input__picker').length, 0);
-  });
+  this.$('.outside-div').click();
+  return wait().then(() => assert.equal(this.$('.date-input__picker').length, 0, 'Picker has been hidden'));
 });
 
 test('if provided, a format string is used', function(assert) {
