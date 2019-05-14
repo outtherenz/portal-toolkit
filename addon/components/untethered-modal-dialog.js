d84cd85f0bf68320d3970a2107e79cd3252a0626
Nathan <genericusernamee@gmail.com>
2019-05-14 14:41:51 +1200

    replace untethered-modal-dialog with modal-dialog

--- addon/components/untethered-modal-dialog.js
+++
@@ -1,9 +0,0 @@
-import ModalDialog from 'ember-modal-dialog/components/modal-dialog';
-import layout from '../templates/components/untethered-modal-dialog';
-
-export default ModalDialog.extend({
-  layout,
-
-  clickOutsideToClose: true,
-  translucentOverlay: true
-});
--- tests/dummy/app/controllers/app-components/untethered-modal-dialog.js
+++ tests/dummy/app/controllers/app-components/untethered-modal-dialog.js
@@ -1,20 +1,4 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  testing: false,
-  works: false,
-  actions: {
-    toggleIsConfirmingTest() {
-      if (this.get('testing')) {
-        this.set('testing', false);
-      } else {
-        this.set('testing', true);
-        this.set('works', false);
-      }
-    },
-    test() {
-      this.set('works', true);
-      this.set('testing', false);
-    }
-  }
 });
